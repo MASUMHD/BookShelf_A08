e7a11a1ce3b87409ff37b381324328bd4293d420
MASUMHD <mrmasum2k4@gmail.com>
2024-03-25 19:29:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className="text-7xl font-bold  bg-rose-600 p-40 rounded-full text-emerald-500 dark:text-emerald-500 text-center" >Welcome to Assignment 08</h1>
+      
+    </>
+  )
+}
+
+export default App
